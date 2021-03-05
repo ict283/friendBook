7acd89010a178ed35416017ca21fcab0cc95d2b7
ict283 <glennlum@icloud.com>
2021-03-05 20:19:13 +0800

    Expose error resulting from shallow copy

--- friendBook/main.cpp
+++ friendBook/main.cpp
@@ -1,12 +1,28 @@
 // For review purposes only!
 
 #include <iostream>
+#include "FriendBook.h"
 
 using namespace std;
 
 int main(int argc, const char * argv[]) {
 
-        //code here
+        FriendBook acc1 ("Amy");
+        acc1.addFriend("Lewis");
+        acc1.addFriend("Gopal");
+        acc1.addFriend("June");
+        acc1.display();
+
+        //Attempt to create a copy using default copy constructor
+        FriendBook acc2(acc1);
+        acc2.display();
+
+        //Attempt to create a copy using '=' operator
+        FriendBook acc3("");
+        acc3=acc1;
+        acc3.display();
+
+        /* Program will crash with a memory allocation error */
 
         return 0;
 }
