2f91efde73800290587e07cb39143120ca42f67d
ict283 <glennlum@icloud.com>
2021-03-05 12:11:53 +0800

    Initial Commit

---
+++ friendBook/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  friendBook
+//
+//  Created by Glenn Lum on 5/3/21.
+//  Copyright © 2021 Glenn Lum. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
