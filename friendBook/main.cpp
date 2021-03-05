b4c5e8014d7aaac0d3ac2c954c6fd1127789dbb3
ict283 <glennlum@icloud.com>
2021-03-05 23:59:59 +0800

    initial commit for this branch

--- friendBook/main.cpp
+++ friendBook/main.cpp
@@ -17,20 +17,12 @@ int main(int argc, const char * argv[]) {
         FriendBook acc2(acc1);
         acc2.display();
 
-<<<<<<< HEAD
         //Attempt to create a copy using default '=' operator
-=======
-        //Attempt to create a copy using '=' operator
->>>>>>> 9ee7940e388863497332be8d5ec320057443b560
         FriendBook acc3("");
         acc3=acc1;
         acc3.display();
 
-<<<<<<< HEAD
         /* Program will crash with a memory allocation error */
-=======
-        /* Program will not crash with a memory allocation error */
->>>>>>> 9ee7940e388863497332be8d5ec320057443b560
 
         return 0;
 }
